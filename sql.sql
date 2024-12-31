6b3e09000d63520a5e2b01748d0d94381c31bba3
Hamza <hamzacoude@gmail.com>
2024-12-31 11:42:28 +0100

    add scren diagramme use case and class

---
+++ sql.sql
@@ -0,0 +1,56 @@
+CREATE DATABASE drive_loc ;
+
+CREATE TABLE Role (
+	idRole INT AUTO_INCREMENT PRIMARY KEY,
+	nomeRole VARCHAR(255) NOT NULL UNIQUE
+    );
+
+INSERT INTO Role (nomeRole) VALUES ('Admin');
+INSERT INTO Role (nomeRole) VALUES ('Client');
+
+CREATE TABLE User (
+    idUser INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(255) NOT NULL ,
+    emeil VARCHAR(255) NOT NULL ,
+    motDepasse VARCHAR(255) NOT NULL  ,
+    adresse VARCHAR(255) , 
+    releId INT ,
+    FOREIGN KEY (roleId) REFERENCES Role(idRole)
+
+);
+
+CREATE TABLE Categorie (
+        idCategorie INT AUTO_INCREMENT PRIMARY KEY ,
+        nom VARCHAR(255) NOT NULL ,
+        description TEXT 
+);
+
+CREATE TABLE Vehicule (
+    idVehicule INT AUTO_INCREMENT PRIMARY KEY ,
+    modele VARCHAR(255) NOT NULL ,
+    prix   DECIMAL(10, 2) NOT NULL ,
+    disponible BOOLEAN NOT NULL ,
+    categorieId INT ,
+    FOREIGN KEY (categorieId) REFERENCES Categorie(idCategorie)
+);
+
+CREATE TABLE Avis (
+    idAvis INT AUTO_INCREMENT PRIMARY KEY ,
+    note INT NOT NULL ,
+    commentaire TEXT ,
+    dateAvis DATE vehiculeId INT ,
+    userId INT ,
+    FOREIGN KEY (vehiculeId) REFERENCES Vehicule(idVehicule),
+    FOREIGN KEY (userId) REFERENCES user(iduser)
+);
+
+CREATE TABLE Reservation (
+    idReservation INT AUTO_INCREMENT PRIMARY KEY,
+    dateDebut DATE NOT NULL,
+    dateFin DATE NOT NULL,
+    statut VARCHAR(255) NOT NULL,
+    vehiculeId INT,
+    userId INT,
+    FOREIGN KEY (vehiculeId) REFERENCES Vehicule(idVehicule),
+    FOREIGN KEY (userId) REFERENCES User(idUser)
+);
